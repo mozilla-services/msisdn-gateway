4f764e71454d9c7ea03322a23c590ebc31d7d866
Rémy HUBSCHER <rhubscher@mozilla.com>
2014-06-10 15:18:27 +0200

    Add the discover endpoint.

--- loadtests/loadtest.py
+++ loadtests/loadtest.py
@@ -14,7 +14,7 @@ from loads.case import TestCase
 PERCENTAGE_OF_MT_FLOW = 50  # Remining are MOMT flows
 PERCENTAGE_OF_WRONG_CODES = 34  # Remining are valid ones.
 PERCENTAGE_OF_SHORT_CODES = 50  # Remining are right ones.
-MAX_OXMEN_TIMEOUT = 10  # Seconds to poll from oxmen.
+MAX_OXMEN_TIMEOUT = 2  # Seconds to poll from oxmen.
 
 PUBLIC_KEY = '{"algorithm":"DS","y":"2bb69e89652d97315f156852c5d1a36c7ebf0fb991a3f812e55c804ca8fa8a09e6f1880de99fcaf4f5417a78a88591679219acda438f9ef4d5afb9d0ae20216414822c6187ec4f180eae6f1f2d2a0f55095e24141c8a5f12344ce4d6319042c168c962de1029a9ac1a528da2d27e4f9550b7c203572a034f077480d9f6e3ef3968902f7547a7374c5e7475dcc81002af3308d09ace22124719a83451cbd872403d6f72cf0b13b5ae0c092a3bf0daf8a26df7d618bc731306ccfbe431c9a30206327bedd0adac35dd5623d017c07aa7fbb991c9364335278b80f1a979d5141fb6cf6cc92849bc6ca5e3c24d25e75f828c0efd57214e37be92d32f2c2208527f20","p":"d6c4e5045697756c7a312d02c2289c25d40f9954261f7b5876214b6df109c738b76226b199bb7e33f8fc7ac1dcc316e1e7c78973951bfc6ff2e00cc987cd76fcfb0b8c0096b0b460fffac960ca4136c28f4bfb580de47cf7e7934c3985e3b3d943b77f06ef2af3ac3494fc3c6fc49810a63853862a02bb1c824a01b7fc688e4028527a58ad58c9d512922660db5d505bc263af293bc93bcd6d885a157579d7f52952236dd9d06a4fc3bc2247d21f1a70f5848eb0176513537c983f5a36737f01f82b44546e8e7f0fabc457e3de1d9c5dba96965b10a2a0580b0ad0f88179e10066107fb74314a07e6745863bc797b7002ebec0b000a98eb697414709ac17b401","q":"b1e370f6472c8754ccd75e99666ec8ef1fd748b748bbbc08503d82ce8055ab3b","g":"9a8269ab2e3b733a5242179d8f8ddb17ff93297d9eab00376db211a22b19c854dfa80166df2132cbc51fb224b0904abb22da2c7b7850f782124cb575b116f41ea7c4fc75b1d77525204cd7c23a15999004c23cdeb72359ee74e886a1dde7855ae05fe847447d0a68059002c3819a75dc7dcbb30e39efac36e07e2c404b7ca98b263b25fa314ba93c0625718bd489cea6d04ba4b0b7f156eeb4c56c44b50e4fb5bce9d7ae0d55b379225feb0214a04bed72f33e0664d290e7c840df3e2abb5e48189fa4e90646f1867db289c6560476799f7be8420a6dc01d078de437f280fff2d7ddf1248d56e1a54b933a41629d6c252983c58795105802d30d7bcd819cf6ef"}'  # NOQA
 
@@ -23,6 +23,9 @@ class TestMSISDN(TestCase):
     omxen_url = "http://ec2-54-203-73-122.us-west-2.compute.amazonaws.com"
 
     def test_all(self):
+        # Discover
+        self.discover()
+
         # Create a token
         self.register()
 
@@ -67,6 +70,14 @@ class TestMSISDN(TestCase):
         code = "%d" % random.randint(0, 999999999)
         code = code.zfill(9)
         return "+33%s" % code
+
+    def discover(self):
+        resp = self.session.post(
+            self.server_url + '/discover',
+            data=json.dumps({"mcc": "204"}),
+            headers={'Content-type': 'application/json'}
+        )
+        self.assertEquals(resp.status_code, 200, resp.content)
 
     def register(self):
         resp = self.session.post(self.server_url + '/register')
@@ -121,6 +132,8 @@ class TestMSISDN(TestCase):
         #  Poll on the omxen message list for this number
         while len(messages) < 1 and \
                 time.time() - start_time < MAX_OXMEN_TIMEOUT:
+            resp = self.session.get(self.omxen_url + '/receive',
+                                    params={"to": self.msisdn.lstrip("+")})
             try:
                 messages = resp.json()
                 self.assertIsInstance(messages, list, messages)
