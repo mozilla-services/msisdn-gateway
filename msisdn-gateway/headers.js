886d9c712667fc116a115e2a3c61a02d64e74334
Rémy HUBSCHER <rhubscher@mozilla.com>
2014-07-03 12:12:59 +0200

    res.on("header") has been deprecated.

--- msisdn-gateway/headers.js
+++ msisdn-gateway/headers.js
@@ -6,14 +6,23 @@
 var conf = require("./config").conf;
 
 module.exports = function headersMiddleware(req, res, next) {
-  res.once('header', function() {
+  /* Make sure we don't decorate the writeHead more than one time. */
+  if (res._headersMiddleware) {
+    next();
+    return;
+  }
+
+  var writeHead = res.writeHead;
+  res._headersMiddleware = true;
+  res.writeHead = function headersWriteHead() {
     if (res.statusCode === 200 || res.statusCode === 401) {
-      res.setHeader('Timestamp', new Date().getTime());
+      res.setHeader('Timestamp', Date.now());
     }
 
     if (res.statusCode === 503 || res.statusCode === 429) {
       res.setHeader('Retry-After', conf.get('retryAfter'));
     }
-  });
+    writeHead.apply(res, arguments);
+  };
   next();
 };
