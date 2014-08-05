7adf8bbf222b8eaa7497f9977b71a88795b684ea
Rémy HUBSCHER <rhubscher@mozilla.com>
2014-08-05 08:03:24 +0200

    Fix #112 — Fix copyright holders.

--- tools/test_cert.js
+++ tools/test_cert.js
@@ -1,9 +1,14 @@
 #!/usr/bin/env node
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 "use strict";
 
 var gen = require("../msisdn-gateway/utils").generateCertificate;
 
 var jwcrypto = require('jwcrypto');
+
 // Make sure to load supported algorithms.
 require('jwcrypto/lib/algs/rs');
 require('jwcrypto/lib/algs/ds');
